19361d2efafdab5e2acee6b881cf925e5f7b0954
김성준 <kimseongjun@gimseongjun-ui-MacBookAir.local>
2023-09-13 17:48:25 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+    id : 1,
+    image : "https://placeimg.com/64/64/1",
+    name : "김성준",
+    birthday : 981112,
+    gender : "남자",
+    job : "대학생"
+  },
+
+  {
+    id : 2,
+    image : "https://placeimg.com/64/64/2",
+    name : "이학명",
+    birthday : 980513,
+    gender : "남자",
+    job : "프로그래머"
+    },
+
+    {
+      id : 3,
+      image : "https://placeimg.com/64/64/3",
+      name : "이준희",
+      birthday : 981222,
+      gender : "남자",
+      job : "디자이너"
+      }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management program</h2>
-    </div>
+    <div>
+      {
+        customers.map((c) => {
+          return (
+            <Customer 
+              key={c.id}
+              id={c.id}
+              image = {c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+            />
+
+          )
+        })
+      }
+     </div>
   );
 }
 
